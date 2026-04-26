86a7b539cadb2f484062e1c8a097370266b2a99c
n3rdrag3 <nerdrag3@proton.me>
2026-04-26 16:22:56 -0400

    add - asteroid onKill split logic

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import *
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,22 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return # asteroid removed from screen
+        else:
+            log_event("asteroid_split")
+            # generate a random angle between 20 and 50 degrees
+            random_angle = random.uniform(20.0, 50.0)
+            new_rotation1 = self.velocity.rotate(random_angle)
+            # opposite direction of angle
+            opposite_random_angle = random_angle * -1
+            new_rotation2 = self.velocity.rotate(opposite_random_angle)
+            new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid1.velocity = new_rotation1 * 1.2
+            asteroid2.velocity = new_rotation2 * 1.2
